1462fd136c0d3dc81e51e903113eeeb8b3e85cf0
lucasllimati <lucasllimati@gmail.com>
2021-02-21 19:00:41 -0300

    Codigo final

---
+++ src/server.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const ejs = require('ejs')
+const path = require('path')
+const puppeteer = require('puppeteer')
+const app = express()
+
+// Base de dados dos passageiros
+const passengers = [
+    {
+        name: "Joyce",
+        flightNumber: "7859",
+        time: "18h00",
+    },
+    {
+        name: "Brock",
+        flightNumber: "7859",
+        time: "18h00",
+    },
+    {
+        name: "Eve",
+        flightNumber: "7859",
+        time: "18h00",
+    },
+];
+
+app.get('/pdf', async(request, response) => {
+
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+
+    await page.goto('http://localhost:3000/', {
+        waitUntil: 'networkidle0'
+    })
+
+    const pdf = await page.pdf({
+        printBackground: true,
+        format: 'Letter'
+    })
+
+    await browser.close()
+
+    response.contentType("application/pdf")
+
+    return response.send(pdf)
+
+})
+
+// callback
+app.get('/', (request, response) => {
+
+    const filePath = path.join(__dirname, "print.ejs")
+    ejs.renderFile(filePath, { passengers }, (err, html) => {
+        if(err) {
+            return response.send('Erro na leitura do arquivo')
+        }        
+
+        // Enviar para o navegador
+        return response.send(html)
+    })    
+})
+
+app.listen(3000)
